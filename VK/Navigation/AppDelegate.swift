1c035250ed8a0559e4bc79cede10f09455d19432
Stanislav Leontyev <arbuspilot@gmail.com>
2021-03-20 13:35:26 +0400

    URL session added

--- VK/Navigation/AppDelegate.swift
+++ VK/Navigation/AppDelegate.swift
@@ -9,10 +9,14 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var appConfiguration: AppConfiguration?
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        appConfiguration = AppConfiguration.urlTwo
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = TabBarController()
